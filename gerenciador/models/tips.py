1418e96c8a2633094729cf347d3235e3e6e3b0c6
Pedro Bizzari <bzrpedro@gmail.com>
2026-06-16 21:34:29 -0300

    feat:: creating class tips to give the user some tips when playing with the generated character

---
+++ gerenciador/models/tips.py
@@ -0,0 +1,20 @@
+from django.db import models
+from .system import System  # Importa o sistema correspondente
+
+class Tips(models.Model):
+   
+    PROFILE_CHOICES = [
+        ('FOR', 'Combatente / Direto'),
+        ('DEX', 'Furtivo / Especialista'),
+        ('INT', 'Erudito / Analítico'),
+        ('CHA', 'Social / Diplomata'),
+    ]
+
+    system = models.ForeignKey(System, on_delete=models.CASCADE, related_name='tips')
+    target_profile = models.CharField(
+        max_length=3, 
+        choices=PROFILE_CHOICES,
+        help_text="Perfil psicológico do jogador detectado no quiz"
+    )
+    suggested_class = models.CharField(max_length=150, help_text="Ex: Mago, Combatente, Ocultista")
+    roleplay_text = models.TextField(help_text="O conselho prático de como o iniciante pode interpretar esse personagem")
